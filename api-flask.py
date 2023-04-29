e77f130e68541f615ec3b2e8494b4c77e15555f9
y0naha <lionafa@gmail.com>
2023-04-29 17:19:02 -0300

    ajuste extra

--- api-flask.py
+++ api-flask.py
@@ -1,45 +1,50 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
-import requests
+from flask import Flask
+import requests as req
+import json
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
-api = Api(app)
 
 # VIA CEP
-class Cep(Resource):
-    def get(self, cep):
-        url = f"https://viacep.com.br/ws/{cep}/json/"
-        response = requests.get(url)
-        data = response.json()
-        return jsonify(data)
-
-api.add_resource(Cep, "/cep/<cep>")
+@app.route('/cep/<cep>', methods=['GET'])
+def search_cep(cep):
+    response = req.get(f'https://viacep.com.br/ws/{cep}/json/')
+    return response.text
 
 # COTAÇÃO DE MOEDAS
+@app.route('/cotacao/<currency>', methods=['GET'])
+def search_currency(currency):
+    resp = req.get(f'https://economia.awesomeapi.com.br/last/{currency}').text
+    resp = json.loads(resp)
+    currency_pair = currency.replace("-", "")
+    response = {
+        "source_currency": resp[currency_pair]["code"],
+        "destination_currency": resp[currency_pair]["codein"],
+        "currency_name": resp[currency_pair]["name"],
+        "max_value": resp[currency_pair]["high"],
+        "min_value": resp[currency_pair]["low"],
+        "currency_date": resp[currency_pair]["create_date"],
+    }
+    return response
 
-@app.route('/convert/<currency>/<value>', methods=['GET'])
-def convert(currency, value):
-    url = f'https://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL,ARS-BRL'
-    response = requests.get(url)
-    data = response.json()
+@app.route('/cotacao/<currency>/<days>', methods=['GET'])
+def search_currency_days(currency, days=1):
+    resp = req.get(f"https://economia.awesomeapi.com.br/json/daily/{currency}/{days}").text
+    resp = json.loads(resp)
+    response = []
+    for data in resp:
+        dt = datetime.fromtimestamp(int(data['timestamp']))
+        br_tz = pytz.timezone('America/Sao_Paulo')
+        dt_br = dt.astimezone(br_tz)
+        formatted_date = dt_br.strftime('%Y-%m-%d %H:%M:%S %Z%z')
+        response.append(
+            {
+                "value": data['high'],
+                "date": formatted_date.split(" ")[0]
+            }
+        )
+    return response
 
-    if currency == 'USD':
-        converted_value = float(value) * float(data['USDBRL']['bid'])
-        result = f'{value} USD = {converted_value:.2f} BRL'
-    elif currency == 'EUR':
-        converted_value = float(value) * float(data['EURBRL']['bid'])
-        result = f'{value} EUR = {converted_value:.2f} BRL'
-    elif currency == 'BTC':
-        converted_value = float(value) * float(data['BTCBRL']['bid'])
-        result = f'{value} BTC = {converted_value:.2f} BRL'
-    elif currency == 'ARS':
-        converted_value = float(value) * float(data['ARSBRL']['bid'])
-        result = f'{value} ARS = {converted_value:.2f} BRL'
-    else:
-        result = f'Invalid currency: {currency}'
-
-    return jsonify({"result": result})
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- script-extra.py
+++ script-extra.py
@@ -4,9 +4,22 @@ import os
 from dotenv import load_dotenv
 import folium
 from streamlit_folium import folium_static
+import plotly.graph_objs as go
+import pandas as pd
+
+# Inicio da sidebar
+st.sidebar.title("Atividade extra Web Mining")
+st.sidebar.caption("Atividade extra para a faculdade Impacta.")
+st.sidebar.caption("Veja o código base deste projeto [aqui](https://github.com/y0naha/web-mining-extra).")
+st.sidebar.markdown("---")
+
+st.sidebar.write(f"Made in <img src='https://streamlit.io/images/brand/streamlit-mark-color.png' width='25px'> by [Larissa Ionafa](https://www.linkedin.com/in/larissa-ionafa/)", unsafe_allow_html=True)
+# Fim da sidebar
+
 
 # VIA CEP
-st.title("Localizador do CEP🔎")
+st.title("API Via CEP 🔎")
+st.subheader("Localizador")
 load_dotenv()
 
 API_KEY_MAPS = os.getenv("API_KEY_MAPS")
@@ -53,14 +66,115 @@ if cep:
     except ValueError as e:
         st.error(str(e))
 
+st.divider()
+
 # COTAÇÃO DE MOEDAS
 
-st.title("Conversor de moedas💰")
+st.title("API de Cotações 💰")
+st.subheader("Cotação de moedas")
 
-currency = st.selectbox("Selecione a moeda de origem:", ("USD", "EUR", "BTC", "ARS"))
-value = st.number_input("Digite o valor a ser convertido:", step=0.01)
+API_URL_MOEDA = "http://localhost:5000/cotacao/"
+
+def get_currency_data(currency):
+    url = API_URL_MOEDA + currency
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+def get_currency_data_days(currency, days):
+    url = API_URL_MOEDA + f"{currency}/{days}"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+def search_currency(currency_from, currency_to, amount):
+    url = f"https://economia.awesomeapi.com.br/last/{currency_from}-{currency_to}"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    exchange_rate = float(data[f"{currency_from.upper()}{currency_to.upper()}"]["high"])
+    converted_amount = round(amount * exchange_rate, 2)
+    return converted_amount
+
+currency = st.selectbox(
+    "Selecione a moeda desejada:",
+    ["USD-BRL", "EUR-BRL", "BTC-BRL", "ETH-BRL"]
+)
+
+if currency:
+    try:
+        currency_data = get_currency_data(currency)
+        st.write("Moeda:", currency_data["currency_name"])
+        st.write("Cotação máxima:", currency_data["max_value"])
+        st.write("Cotação mínima:", currency_data["min_value"])
+        st.write("Data:", currency_data["currency_date"])
+
+        days = st.slider(
+            "Selecione o número de dias para visualizar o histórico:",
+            min_value=1,
+            max_value=30,
+            value=15,
+        )
+
+        currency_data_days = get_currency_data_days(currency, days)
+
+        df = pd.DataFrame(currency_data_days)
+        df["date"] = pd.to_datetime(df["date"])
+        fig = go.Figure(data=go.Scatter(x=df["date"], y=df["value"]))
+        fig.update_layout(title=f"Histórico de cotação da moeda {currency_data['currency_name']} nos últimos {days} dias")
+        st.plotly_chart(fig)
+
+    except ValueError as e:
+        st.error(str(e))
+
+st.subheader("Converter moedas")
+
+currency_list = [
+    "BRL",
+    "USD",
+    "EUR",
+    "BTC",
+    "JPY",
+    "GBP",
+    "AUD",
+    "CAD",
+    "CHF",
+    "CNY",
+    "ARS",
+    "HKD",
+    "ILS",
+    "INR",
+    "KRW",
+    "MXN",
+    "NOK",
+    "NZD",
+    "PLN",
+    "SEK",
+    "SGD",
+    "THB",
+    "TRY",
+    "ZAR"
+]
+
+currency_from = st.selectbox(
+    "Selecione a moeda de origem",
+    currency_list
+)
+
+currency_to = st.selectbox(
+    "Selecione a moeda de destino",
+    currency_list
+)
+
+amount = st.number_input("Insira o valor a ser convertido")
+
 
 if st.button("Converter"):
-    response = requests.get(f"http://localhost:5000/convert/{currency}/{value}")
-    result = response.json()["result"]
-    st.success(result)
+    conversion_data = search_currency(currency_from)
+    source_currency = conversion_data["source_currency"]
+    destination_currency = conversion_data["destination_currency"]
+    conversion_rate = float(conversion_data["max_value"])
+    converted_amount = amount * conversion_rate
+    st.write(f"{amount:.2f} {source_currency} = {converted_amount:.2f} {destination_currency}")
